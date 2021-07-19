66ea2e63c21ee14dc8d4394ded319e2d67a3b3a1
Nicolas Oyeleye <nicolas.oyeleye2@mail.dcu.ie>
2021-07-19 12:38:59 +0100

    Pulling from gh

--- 1_pre-process/test1.py
+++ 1_pre-process/test1.py
@@ -14,48 +14,33 @@ def main():
     df_polar = import_polar(path_polar)
     df_bio = import_bio(path_bio)
 
-    # defining start and finish times as a datetime object
     # start time for Edrick DC1
-    start_time = dt.strptime('13/06/2019 13:38:51', '%d/%m/%Y %H:%M:%S')
+    start_time = '13:38:51'
     # finish time for Edrick DC1
-    finish_time = dt.strptime('13/06/2019 13:52:15', '%d/%m/%Y %H:%M:%S')
+    finish_time = '13:52:15'
 
-    df_bio = df_bio.between_time(start_time.time(), finish_time.time())
-    df_polar = df_polar.between_time(start_time.time(), finish_time.time())
-
+    # selecting data between start and finish times
+    df_polar = df_polar.between_time(start_time, finish_time)
+    df_bio = df_bio.between_time(start_time, finish_time)
 
     #timing the program
     #start = time()
-    #polar_avec(df_polar, projectdir, "Edrick", "1", '2019-6-13_RR_Edrick.csv')
-    #bio_avec(df_bio)
-    #polar_bio(df_polar, df_bio)
-    df_polar_avec = polar_avec(df_polar, projectdir, "Edrick", "1", '2019-6-13_RR_Edrick.csv')
-    df_bio_avec = bio_avec(df_bio, projectdir, "Edrick", "1", '2019-6-13_RR_Edrick.csv')
-    polar_bio_avec(df_polar_avec, df_bio_avec)
+    df_polar = polar_avec(df_polar, projectdir, "Edrick", "1", '2019-6-13_RR_Edrick.csv')
     #end = time()
-    #print(df_polar.columns)
-
     #print('{} seconds'.format(end - start))
 
-# Raw bioharness and polar
-def polar_bio(polar, bio):
-        plt.plot(polar.index, polar[' rr'])
-        plt.plot(bio.index, bio['RtoR'])
-        plt.legend(["Polar", "Bioharness"])
-        plt.title("Raw Polar Vs Bioharness")
-        plt.show()
+    df_bio = bio_avec(df_bio, projectdir, "Edrick", "1", '2019-6-13_RR_Edrick.csv')
 
-# bioharness and polar after avec
-def polar_bio_avec(polar, bio):
-        plt.plot(polar.index, polar['removed out'])
-        plt.plot(bio.index, bio['removed out'])
-        plt.legend(["Polar", "Bioharness"])
-        plt.title("Polar Vs Bioharness without Outliers")
-        plt.show()
+    plots.raw_avec(df_polar, 'Polar')
+    plots.raw_avec(df_bio, 'Bioharness')
+
+    plots.polar_bio(df_polar, df_bio, 'rr_raw')
+    plots.polar_bio(df_polar, df_bio, 'rr_avec')
+
 
 # polar raw and avec
 def polar_avec(df, dir, subject, dc, filename):
-    df['removed out'] = avec(list(df[' rr']))
+    df['rr_avec'] = avec(list(df['rr_raw']))
 
     # Creating folder structure
     subject_dir = os.path.join(dir, "2-data-processed", subject)
@@ -69,17 +54,11 @@ def polar_avec(df, dir, subject, dc, filename):
 
     path = os.path.join(projectdir, '2-data-processed', subject, '{}_Polar'.format(dc), filename)
     df.to_csv(path, index=True)
-    plt.plot(df.index, df[' rr'])
-    plt.plot(df.index, df['removed out'])
-    plt.legend(["Raw Polar", "Outliers Removed"])
-    plt.title("Polar Graph")
-    plt.show()
-
     return df
 
 # bioharness raw and avec
 def bio_avec(df, dir, subject, dc, filename):
-    df["removed out"] = avec(list(df['RtoR']))
+    df['rr_avec'] = avec(list(df['rr_raw']))
     # Creating folder structure
     subject_dir = os.path.join(dir, "2-data-processed", subject)
     if not os.path.exists(subject_dir):
@@ -92,11 +71,6 @@ def bio_avec(df, dir, subject, dc, filename):
 
     path = os.path.join(projectdir, '2-data-processed', subject, '{}_Bioharness'.format(dc), filename)
     df.to_csv(path, index=True)
-    plt.plot(df.index, df['RtoR'])
-    plt.plot(df.index, df['removed out'])
-    plt.legend(["Raw Bioharness", "Outliers Removed"])
-    plt.title("Bioharness Graph")
-    plt.show()
 
     return df
 
@@ -104,6 +78,8 @@ def bio_avec(df, dir, subject, dc, filename):
 def import_polar(path):
     parser = lambda x: datetime.strptime(x,'%Y-%m-%d %H:%M:%S +0000')
     df = pd.read_csv(path, index_col ='date', date_parser = parser, dayfirst = True)
+    df.rename(columns = {' rr': 'rr_raw'}, inplace = True)
+    df.drop(' since start', axis = 1, inplace = True)
     return df
 
 # Function to import bioharness data
@@ -112,6 +88,7 @@ def import_bio(path):
     df = pd.read_csv(path, index_col ='Timestamp' , usecols = ['Timestamp', 'RtoR'], date_parser = parser, dayfirst = True)
     df.drop_duplicates(['RtoR'], keep = 'first', inplace = True)
     df['RtoR'] = round(df['RtoR'].abs()*1000)
+    df.rename(columns = {'RtoR': 'rr_raw'}, inplace = True)
     return df
 
 if __name__ == '__main__':
